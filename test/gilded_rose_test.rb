8231bd0a4e4003cee36641cc69c6f8dbd87b7699
Piotr Romańczuk <piotr.romanczuk@gmail.com>
2020-11-01 17:23:41 +0100

    make 'manual' test from texttest_fixture.rb a proper unit test; remove now unneeded fixture file

--- test/gilded_rose_test.rb
+++ test/gilded_rose_test.rb
@@ -1,5 +1,6 @@
 require File.join(File.dirname(__FILE__), '../gilded_rose')
 require 'minitest/autorun'
+require 'pry'
 
 class TestUntitled < Minitest::Test
 
@@ -9,4 +10,62 @@ class TestUntitled < Minitest::Test
     assert_equal items[0].name, "foo"
   end
 
+  def test_report
+    report_lines = []
+    items = [
+      Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20),
+      Item.new(name="Aged Brie", sell_in=2, quality=0),
+      Item.new(name="Elixir of the Mongoose", sell_in=5, quality=7),
+      Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+      Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+      Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+      Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+      Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+      # This Conjured item does not work properly yet
+      Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
+    ]
+
+    days = 2
+
+    gilded_rose = GildedRose.new items
+    (0...days).each do |day|
+      report_lines << "-------- day #{day} --------"
+      report_lines << "name, sellIn, quality"
+      items.each do |item|
+        report_lines << item.to_s
+      end
+      report_lines << ""
+      gilded_rose.update_quality
+    end
+
+
+    expected = <<~EOT.split("\n") + [""]
+      -------- day 0 --------
+      name, sellIn, quality
+      +5 Dexterity Vest, 10, 20
+      Aged Brie, 2, 0
+      Elixir of the Mongoose, 5, 7
+      Sulfuras, Hand of Ragnaros, 0, 80
+      Sulfuras, Hand of Ragnaros, -1, 80
+      Backstage passes to a TAFKAL80ETC concert, 15, 20
+      Backstage passes to a TAFKAL80ETC concert, 10, 49
+      Backstage passes to a TAFKAL80ETC concert, 5, 49
+      Conjured Mana Cake, 3, 6
+
+      -------- day 1 --------
+      name, sellIn, quality
+      +5 Dexterity Vest, 9, 19
+      Aged Brie, 1, 1
+      Elixir of the Mongoose, 4, 6
+      Sulfuras, Hand of Ragnaros, 0, 80
+      Sulfuras, Hand of Ragnaros, -1, 80
+      Backstage passes to a TAFKAL80ETC concert, 14, 21
+      Backstage passes to a TAFKAL80ETC concert, 9, 50
+      Backstage passes to a TAFKAL80ETC concert, 4, 50
+      Conjured Mana Cake, 2, 5
+      EOT
+
+    assert_equal expected, report_lines
+
+  end
 end
