47358d7afcb3d314f662c47f0c2625268eeb8235
Piotr Romańczuk <piotr.romanczuk@gmail.com>
2020-11-02 19:18:42 +0100

    Merge 2 big if/else structures when they have the same types

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -12,32 +12,32 @@ class GildedRose
           decrease_quality item
         end
         item.sell_in = item.sell_in - 1
+        if item.sell_in < 0
+          if item.quality > 0
+            decrease_quality item
+          end
+        end
       elsif aged_brie?(item)
         if quality_less_than_50?(item)
           increase_quality item
         end
         item.sell_in = item.sell_in - 1
-      elsif backstage_pass?(item)
-        _handle_backstage_pass(item)
-      end
-
-      if aged_brie?(item)
         if item.sell_in < 0
           if quality_less_than_50?(item)
             increase_quality item
           end
         end
       elsif backstage_pass?(item)
+        _handle_backstage_pass(item)
         if item.sell_in < 0
           item.quality = item.quality - item.quality
         end
+      end
+
+      if aged_brie?(item)
+      elsif backstage_pass?(item)
       elsif sulfuras?(item)
       elsif generic?(item)
-        if item.sell_in < 0
-          if item.quality > 0
-            decrease_quality item
-          end
-        end
       end
     end
   end
